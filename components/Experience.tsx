97f00f4e12ae408059d760a9aeaec2a63fdbe006
Uttam Jangir <uttamjangir14@gmail.com>
2024-12-30 12:03:11 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,9 +8,9 @@ const Experience = () => {
         Under Construction: Experience Coming Soon
       </h2>
       <p className="text-sm text-slate-400 my-2">
-        "I&apos;m currently building up my experience one project at a time and
+        &quot;I&apos;m currently building up my experience one project at a time and
         I have not yet had a chance to gain some professional experience. Until
-        then, check out the projects below."
+        then, check out the projects below.&quot;
       </p>
     </div>
   );
